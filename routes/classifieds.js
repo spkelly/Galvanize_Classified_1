8f46a5f03a7e92743293c9296ea2a54361f7674e
spkelly <spkelly18@gmail.com>
2017-05-17 13:20:32 -0600

    patch post and delete routes working

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -8,22 +8,49 @@ const router = express.Router();
 
 // YOUR CODE HERE
 
-router.get('/', (req,res) =>{
+router.get('/', (req, res) => {
   knex('classifieds')
-    .select('id','title','description','item_image','price')
-    .then((data)=>{
-      res.send(data);
-    })
-})
+    .select('id', 'title', 'description', 'item_image', 'price')
+    .then((data) => res.send(data));
+});
 
-router.get('/:id', (req,res) =>{
-  let id = req.params.id;
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
 
   knex('classifieds')
-    .where('id',id)
-    .select('id','title','description','item_image','price')
-    .then((data)=>{
-      res.send(data[0]);
-    })
-})
+    .where('id', id)
+    .select('id', 'title', 'description', 'item_image', 'price')
+    .then((data) => res.send(data[0]));
+});
+
+router.post('/', (req, res) => {
+  const ad = req.body;
+
+  knex('classifieds')
+    .insert(ad)
+    .returning(['id','title', 'description', 'item_image', 'price'])
+    .then((data) => res.send(data[0]));
+});
+
+router.patch('/:id', (req, res) => {
+  const id = req.params.id;
+  const ad = req.body;
+
+  knex('classifieds')
+    .where('id', id)
+    .update(ad)
+    .returning(['id', 'title', 'description', 'item_image', 'price'])
+    .then((data) => res.send(data[0]));
+});
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  knex('classifieds')
+    .where('id', id)
+    .del()
+    .returning(['id', 'title', 'description', 'item_image', 'price'])
+    .then((data) => res.send(data[0]));
+});
+
 module.exports = router;
