e4574a4e8e023cb587f39d867e6410dc5b476f6a
spkelly <spkelly18@gmail.com>
2017-05-17 13:05:36 -0600

    classified get routes are working

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,28 @@
 'use strict';
 
 const express = require('express');
+const knex = require('../knex');
 
 const router = express.Router();
 
 // YOUR CODE HERE
 
+router.get('/', (req,res) =>{
+  knex('classifieds')
+    .select('id','title','description','item_image','price')
+    .then((data)=>{
+      res.send(data);
+    })
+})
+
+router.get('/:id', (req,res) =>{
+  let id = req.params.id;
+
+  knex('classifieds')
+    .where('id',id)
+    .select('id','title','description','item_image','price')
+    .then((data)=>{
+      res.send(data[0]);
+    })
+})
 module.exports = router;
